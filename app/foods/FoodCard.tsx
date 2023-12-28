6386e466f40653605a64dc0e0e09584d534ba07a
longquan98@gmail.com <longquan98@gmail.com>
2023-12-28 18:20:09 -0500

    Update the FoodCard component

--- app/foods/FoodCard.tsx
+++ app/foods/FoodCard.tsx
@@ -1,25 +1,30 @@
 import { Food } from "@prisma/client";
-import { Button, Card, Flex, Text } from "@radix-ui/themes";
+import { Box, Button, Card, Flex, Text } from "@radix-ui/themes";
 import Image from "next/image";
 import Link from "next/link";
 
 const FoodCard = ({ food: { imgSrc, name, stock, id } }: { food: Food }) => {
   return (
-    <Card style={{ maxWidth: 200, height: "auto" }}>
+    <Box className="px-0 py-0 pb-3 border border-slate-400 rounded-md overflow-hidden h-auto max-w-52">
       <Flex direction="column" justify="start" align="center">
-        <Image src={imgSrc} alt="food image" width="200" height="20" />
-        <Text>
+        <Image src={imgSrc} alt="food image" width="208" height="20" />
+        <Text mt="2" align="center">
           <Link href={`/foods/${id}`} className="font-normal hover:font-medium">
             {name}
           </Link>
         </Text>
-        <Button mt="2" size={{ initial: "3", sm: "3" }} disabled={stock === 0}>
+        <Button
+          mt="2"
+          mx="2"
+          size={{ initial: "3", sm: "3" }}
+          disabled={!stock}
+        >
           <Text size={{ initial: "1", sm: "3" }}>
-            {stock > 0 ? "Add To Card" : "Out Of Stock"}
+            {stock ? "Add To Card" : "Out Of Stock"}
           </Text>
         </Button>
       </Flex>
-    </Card>
+    </Box>
   );
 };
 
