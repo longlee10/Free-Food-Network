6723f7cd9d4591eb78f7f790138081b7252109c3
longquan98@gmail.com <longquan98@gmail.com>
2023-12-28 18:28:03 -0500

    Build the food details page

--- app/foods/[id]/page.tsx
+++ app/foods/[id]/page.tsx
@@ -1,7 +1,53 @@
-import React from "react";
+import prisma from "@/prisma/client";
+import { Box, Text, Flex, Grid, Strong, Heading } from "@radix-ui/themes";
+import Image from "next/image";
 
-const FoodDetailsPage = () => {
-  return <div>FoodDetailsPage</div>;
+interface Props {
+  params: { id: string };
+}
+
+const FoodDetailsPage = async ({ params: { id } }: Props) => {
+  const food = await prisma.food.findUnique({
+    where: { id: parseInt(id) },
+  });
+
+  const details = [
+    { label: "Description: ", text: food?.description },
+    { label: "Category: ", text: food?.category },
+    { label: "Brand: ", text: food?.brand },
+    { label: "Origin: ", text: food?.origin },
+    {
+      label: "Availability: ",
+      text: food?.stock ? "Available" : "Unavailable",
+    },
+  ];
+
+  if (!food) return null;
+  return (
+    <Box className="border border-slate-300 rounded-md" px="5" py="3">
+      <Grid gap="5" columns={{ initial: "1", sm: "2" }}>
+        <Box>
+          <Heading>{food.name.toUpperCase()}</Heading>
+          <Image
+            src={food.imgSrc}
+            alt="food"
+            width="300"
+            height="250"
+            style={{ marginTop: "1rem" }}
+          ></Image>
+        </Box>
+        <Flex direction="column" pl={{ initial: "0", md: "5" }}>
+          <Heading mb="3">ITEM INFORMATION:</Heading>
+          {details.map((detail) => (
+            <Text>
+              <Strong>{detail.label}</Strong>
+              {detail.text}
+            </Text>
+          ))}
+        </Flex>
+      </Grid>
+    </Box>
+  );
 };
 
 export default FoodDetailsPage;
