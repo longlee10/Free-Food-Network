dd591513616d183de322e21279d80ef6cc19e6b1
longquan98@gmail.com <longquan98@gmail.com>
2023-12-28 16:36:52 -0500

    Render food items from mysql database

--- app/foods/FoodCard.tsx
+++ app/foods/FoodCard.tsx
@@ -1,23 +1,13 @@
+import { Food } from "@prisma/client";
 import { Button, Card, Flex, Text } from "@radix-ui/themes";
 import Image from "next/image";
 import Link from "next/link";
 
-interface Food {
-  id: number;
-  src: string;
-  name: string;
-  stock: number;
-}
-
-interface Props {
-  food: Food;
-}
-
-const FoodCard = ({ food: { src, name, stock, id } }: Props) => {
+const FoodCard = ({ food: { imgSrc, name, stock, id } }: { food: Food }) => {
   return (
     <Card style={{ maxWidth: 200, height: "auto" }}>
       <Flex direction="column" justify="start" align="center">
-        <Image src={src} alt="food image" width="200" height="20" />
+        <Image src={imgSrc} alt="food image" width="200" height="20" />
         <Text>
           <Link href={`/foods/${id}`} className="font-normal hover:font-medium">
             {name}
--- app/foods/page.tsx
+++ app/foods/page.tsx
@@ -1,18 +1,9 @@
-import { Container, Grid } from "@radix-ui/themes";
+import { Grid } from "@radix-ui/themes";
 import FoodCard from "./FoodCard";
+import prisma from "@/prisma/client";
 
-const FoodsPage = () => {
-  const foods = [
-    { id: 1, name: "Green Lentils", src: "/00062639364069.jpg", stock: 10 },
-    { id: 2, name: "Brown Eggs", src: "/00062639364069.jpg", stock: 10 },
-    { id: 3, name: "Potatoes", src: "/00062639364069.jpg", stock: 10 },
-    { id: 4, name: "Orange", src: "/00062639364069.jpg", stock: 0 },
-    { id: 5, name: "Milk", src: "/00062639364069.jpg", stock: 10 },
-    { id: 6, name: "Brown Eggs", src: "/00062639364069.jpg", stock: 0 },
-    { id: 7, name: "Potatoes", src: "/00062639364069.jpg", stock: 10 },
-    { id: 9, name: "Orange", src: "/00062639364069.jpg", stock: 10 },
-    { id: 9, name: "Milk", src: "/00062639364069.jpg", stock: 10 },
-  ];
+const FoodsPage = async () => {
+  const foods = await prisma.food.findMany();
 
   return (
     <Grid gap="5" columns={{ initial: "2", sm: "3", md: "4", lg: "5" }}>
