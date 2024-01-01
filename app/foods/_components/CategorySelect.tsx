e7a79760c53811d77334877956748f8639599386
longquan98@gmail.com <longquan98@gmail.com>
2023-12-31 23:00:16 -0500

    Implement filter by category

---
+++ app/foods/_components/CategorySelect.tsx
@@ -0,0 +1,42 @@
+"use client";
+import { Select } from "@radix-ui/themes";
+import { useSearchParams, useRouter } from "next/navigation";
+
+const CategorySelect = () => {
+  const categories = [
+    { value: "protein", label: "Protein" },
+    { value: "vegetables", label: "Vegetables" },
+    { value: "fruits", label: "Fruits" },
+    { value: "carb", label: "Carb" },
+    { value: "dairy", label: "Dairy" },
+  ];
+  const searchParams = useSearchParams();
+  const router = useRouter();
+
+  return (
+    <Select.Root
+      defaultValue=""
+      onValueChange={(category) => {
+        const params = new URLSearchParams();
+        params.append("category", category);
+        const query = params.size ? `?${params.toString()}` : "";
+        router.push(`/foods/${query}`);
+      }}
+    >
+      <Select.Trigger />
+      <Select.Content>
+        <Select.Group>
+          <Select.Item value="">Filter by: All Category</Select.Item>
+          {categories.map((category) => (
+            <Select.Item
+              key={category.value}
+              value={category.value}
+            >{`Filter by Category: ${category.label}`}</Select.Item>
+          ))}
+        </Select.Group>
+      </Select.Content>
+    </Select.Root>
+  );
+};
+
+export default CategorySelect;
--- app/foods/page.tsx
+++ app/foods/page.tsx
@@ -1,16 +1,25 @@
-import { Button, Grid } from "@radix-ui/themes";
+import { Button, Flex, Grid } from "@radix-ui/themes";
 import FoodCard from "./FoodCard";
 import prisma from "@/prisma/client";
 import Link from "next/link";
+import CategorySelect from "./_components/CategorySelect";
 
-const FoodsPage = async () => {
-  const foods = await prisma.food.findMany();
+interface Props {
+  searchParams: { category: string };
+}
+
+const FoodsPage = async ({ searchParams }: Props) => {
+  const category = searchParams.category ? searchParams.category : undefined;
+  const foods = await prisma.food.findMany({ where: { category: category } });
 
   return (
     <>
-      <Button mb="3">
-        <Link href="/foods/new">Add NewFood</Link>
-      </Button>
+      <Flex mb="3" justify="between">
+        <CategorySelect />
+        <Button>
+          <Link href="/foods/new">Add New Food</Link>
+        </Button>
+      </Flex>
       <Grid gap="5" columns={{ initial: "2", sm: "3", md: "4", lg: "5" }}>
         {foods.map((food) => (
           <FoodCard food={food} key={food.id} />
