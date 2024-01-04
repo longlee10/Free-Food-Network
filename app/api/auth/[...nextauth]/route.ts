398ef728f0bc4f3e4169dfde576daf10076e2a8c
longquan98@gmail.com <longquan98@gmail.com>
2024-01-04 16:08:09 -0500

    set up prisma adapter and create tables for user auth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,10 @@
-import NextAuth from "next-auth";
+import NextAuth, { NextAuthOptions } from "next-auth";
+import prisma from "@/prisma/client";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 
-const handler = NextAuth({});
+const handler: NextAuthOptions = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [],
+});
 
 export { handler as GET, handler as POST };
