7cdf5ce760c922e0df4645bdcbb17d270b796f57
longquan98@gmail.com <longquan98@gmail.com>
2024-01-04 17:44:17 -0500

    Add sign in / sign up link to nav bar and build the sign up form

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -6,12 +6,18 @@ const NavBar = () => {
   return (
     <nav className="border-b mb-3 px-3 py-3">
       <Container>
-        <Flex gap="5" align="center">
-          <Link href="/">
-            <MdFoodBank size="25" />
-          </Link>
-          <Link href="/">Free Food Network</Link>
-          <Link href="/foods">Foods</Link>
+        <Flex justify="between">
+          <Flex gap="5" align="center">
+            <Link href="/">
+              <MdFoodBank size="25" />
+            </Link>
+            <Link href="/">Free Food Network</Link>
+            <Link href="/foods">Foods</Link>
+          </Flex>
+          <Flex gap="5">
+            <Link href="/api/auth/signin">Sign In</Link>
+            <Link href="/user/register">Sign Up</Link>
+          </Flex>
         </Flex>
       </Container>
     </nav>
