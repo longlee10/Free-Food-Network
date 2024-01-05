b1d900b8da6dd8a6ae73921fb5e4b280f27940d5
longquan98@gmail.com <longquan98@gmail.com>
2024-01-04 22:43:54 -0500

    Handling error in registration

--- app/user/register/RegistrationForm.tsx
+++ app/user/register/RegistrationForm.tsx
@@ -1,7 +1,9 @@
 "use client";
 import Spinner from "@/app/components/Spinner";
+import ErrorMessage from "@/app/foods/_components/ErrorMessage";
 import { userRegistrationSchema } from "@/app/validationSchema";
-import { Box, Text, TextField, Button, Flex } from "@radix-ui/themes";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Box, Text, TextField, Button, Flex, Callout } from "@radix-ui/themes";
 import axios from "axios";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
@@ -12,38 +14,56 @@ import { z } from "zod";
 type RegistrationFormData = z.infer<typeof userRegistrationSchema>;
 
 const RegistrationForm = () => {
-  const { register, handleSubmit } = useForm<RegistrationFormData>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<RegistrationFormData>({
+    resolver: zodResolver(userRegistrationSchema),
+  });
   const router = useRouter();
   const [submitting, setSubmit] = useState(false);
+  const [error, setError] = useState("");
 
   return (
     <Box className="w-1/2 m-auto text-center border-solid border-2 p-3 rounded-md">
+      {error && (
+        <Callout.Root mb="3" color="red">
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
       <Text className="font-bold text-2xl">WELCOME TO FREE FOOD NETWORK!</Text>
       <form
         className="mt-3"
         onSubmit={handleSubmit(async (data) => {
           try {
             setSubmit(true);
-            axios.post("/api/user/register", data);
+            await axios.post("/api/user/register", data);
             router.push("/");
             router.refresh();
           } catch (err) {
             setSubmit(false);
+            setError("An unexpected error occurred.");
           }
         })}
       >
+        <ErrorMessage>{errors.email?.message}</ErrorMessage>
         <TextField.Root mb="3">
           <TextField.Input
             placeholder="Enter email..."
             {...register("email")}
           />
         </TextField.Root>
+
+        <ErrorMessage>{errors.name?.message}</ErrorMessage>
         <TextField.Root mb="3">
           <TextField.Input
             placeholder="Enter username..."
             {...register("name")}
           />
         </TextField.Root>
+
+        <ErrorMessage>{errors.password?.message}</ErrorMessage>
         <TextField.Root mb="3">
           <TextField.Input
             placeholder="Enter password..."
